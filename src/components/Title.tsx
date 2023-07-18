928c77ecd81c50e95e610b7e770bb5b0b29565a9
GVisser1 <gvisser.business@gmail.com>
2023-07-18 12:05:09 +0200

    Update text components

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -3,14 +3,12 @@ import { FC, PropsWithChildren } from "react";
 
 export interface TitleProps {
   className?: string;
-  size?: "base" | "lg" | "xl" | "2xl" | "3xl" | "4xl" | "5xl";
   color?: "medium" | "dark" | "all-white" | "all-dark";
-  as?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+  as?: "h1" | "h2" | "h3" | "h4";
 }
 
 export const Title: FC<PropsWithChildren<TitleProps>> = ({
   as = "h2",
-  size = "base",
   color = "dark",
   className,
   children,
@@ -23,14 +21,12 @@ export const Title: FC<PropsWithChildren<TitleProps>> = ({
     color === "medium" && "text-gray-500 dark:text-gray-400",
     color === "dark" && "text-gray-700 dark:text-white",
 
-    size === "base" && "text-base",
-    size === "lg" && "text-lg",
-    size === "xl" && "text-xl",
-    size === "2xl" && "text-2xl",
-    size === "3xl" && "text-3xl",
-    size === "4xl" && "text-4xl",
-    size === "5xl" && "text-5xl",
-    className
+    as === "h1" && "text-6xl",
+    as === "h2" && "text-4xl",
+    as === "h3" && "text-xl",
+    as === "h4" && "text-base",
+
+    className,
   );
 
   return <Tag className={classes}>{children}</Tag>;
