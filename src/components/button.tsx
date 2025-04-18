96c1a7f929289053d9c2453657d55211f442d74e
GVisser1 <gvisser.business@gmail.com>
2025-04-18 15:22:52 +0200

    Increase line height button text, make hero h1 larger, disable nextjs dev mode

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -88,13 +88,12 @@ const ButtonIcon = (props: ButtonIconProps): JSX.Element => <Icon name={props.ic
 
 const BaseButton = (props: PropsWithChildren<ButtonProps | LinkProps>): JSX.Element => {
   const classes = clsx(
-    "inline-flex shrink-0 items-center rounded-sm p-2 select-none focus-visible:outline",
+    "inline-flex shrink-0 items-center rounded-sm select-none focus-visible:outline",
     "data-[active=true]:bg-zinc-100 data-[active=true]:font-semibold dark:data-[active=true]:bg-zinc-800",
     "disabled:cursor-not-allowed disabled:text-zinc-500 dark:disabled:bg-zinc-900",
-    props.size === "lg" ? "h-12 px-3 py-2" : "h-9 p-2",
+    props.size === "lg" ? "h-12 px-3" : "h-9 px-2",
     props.fullWidth && "w-full",
     !props.disabled && !props.active && variantClasses[props.variant],
-    // !("icon" in props) ? "px-4 py-2" : "px-2 py-2",
     props.className,
   );
 
